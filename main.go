98d4a8d1e5e96fa110728109cf93fbe474cc0a4e
San3ch3g <117848931+San3ch3g@users.noreply.github.com>
2023-11-14 15:00:48 +0600

    Update main.go

--- main.go
+++ main.go
@@ -16,14 +16,18 @@ import (
 
 const PORT = ":8080"
 
+// Data структура для представления данных из базы данных
 type Data struct {
 	Id      int    `json:"id"`
 	Content string `json:"title"`
 	Status  bool   `json:"completed"`
 }
 
+// data обработчик для получения данных из базы данных
 func data(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Установка соединения с базой данных
 	db, err := sql.Open("mysql", "your_user:your_password@tcp(host:port)/your_data_base")
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
@@ -32,20 +36,23 @@ func data(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer db.Close()
+
+	// Проверка соединения с базой данных
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
+	// Выполнение запроса к базе данных для получения данных
 	rows, err := db.Query("SELECT * FROM Todos")
 	if err != nil {
 		panic(err)
 	}
-
 	defer rows.Close()
 
 	var dataList []Data
 
+	// Итерация по результатам запроса и заполнение структуры Data
 	for rows.Next() {
 		var column1 int
 		var column2 string
@@ -62,6 +69,7 @@ func data(w http.ResponseWriter, r *http.Request) {
 		dataList = append(dataList, data)
 	}
 
+	// Преобразование данных в формат JSON и отправка клиенту
 	bytes, err := json.Marshal(dataList)
 	if err != nil {
 		panic(err)
@@ -69,22 +77,27 @@ func data(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
+// update обработчик для обновления данных в базе данных
 func update(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Чтение тела запроса
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
 		return
 	}
-
 	defer r.Body.Close()
 
+	// Разбор JSON из тела запроса в структуру Data
 	var data Data
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		http.Error(w, "Ошибка разбора JSON", http.StatusBadRequest)
 		return
 	}
+
+	// Установка соединения с базой данных
 	db, err := sql.Open("mysql", "your_user:your_password@tcp(host:port)/your_data_base")
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
@@ -92,6 +105,9 @@ func update(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+
+	// Преобразование статуса в строку
 	var num string = strconv.Itoa(data.Id)
 	var status string
 	if data.Status {
@@ -99,32 +115,40 @@ func update(w http.ResponseWriter, r *http.Request) {
 	} else {
 		status = "0"
 	}
+
+	// Формирование и выполнение SQL-запроса на обновление данных
 	var query = "UPDATE `todos` SET `completed`= " + status + " WHERE `id` =" + num
 	rows, err := db.Query(query)
 	if err != nil {
 		panic(err)
 	}
-
 	defer rows.Close()
+
+	// Отправка успешного статуса
 	w.WriteHeader(http.StatusOK)
 }
 
+// createReq обработчик для создания новой записи в базе данных
 func createReq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Чтение тела запроса
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
 		return
 	}
-
 	defer r.Body.Close()
 
+	// Разбор JSON из тела запроса в структуру Data
 	var data Data
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		http.Error(w, "Ошибка разбора JSON", http.StatusBadRequest)
 		return
 	}
+
+	// Установка соединения с базой данных
 	db, err := sql.Open("mysql", "your_user:your_password@tcp(host:port)/your_data_base")
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
@@ -132,32 +156,36 @@ func createReq(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	query := "INSERT INTO `todos` (title, completed) VALUES ('" + data.Content + "', '" + strconv.FormatBool(data.Status) + "')"
+	defer db.Close()
 
+	// Формирование и выполнение SQL-запроса на создание новой записи
+	query := "INSERT INTO `todos` (title, completed) VALUES ('" + data.Content + "', '" + strconv.FormatBool(data.Status) + "')"
 	rows, err := db.Query(query)
 	if err != nil {
 		panic(err)
 	}
-
 	defer rows.Close()
 }
 
+// delete обработчик для удаления записи из базы данных
 func delete(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	// Проверка метода запроса
 	if r.Method != http.MethodPost {
 		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
 		return
 	}
 
+	// Чтение тела запроса
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
 		return
 	}
-
 	defer r.Body.Close()
 
+	// Разбор JSON из тела запроса в структуру Data
 	var data Data
 	err = json.Unmarshal(body, &data)
 	if err != nil {
@@ -165,6 +193,7 @@ func delete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Установка соединения с базой данных
 	db, err := sql.Open("mysql", "your_user:your_password@tcp(host:port)/your_data_base")
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
@@ -172,26 +201,36 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
+	// Преобразование ID в строку
 	var num string = strconv.Itoa(data.Id)
+
+	// Формирование и выполнение SQL-запроса на удаление записи
 	var query = "DELETE FROM `todos` WHERE `id` =" + num
 	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	db.Close()
+	// Отправка успешного статуса
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-
+	// Создание маршрутизатора
 	mux := http.NewServeMux()
+
+	// Установка обработчиков для различных запросов
 	mux.HandleFunc("/data", data)
 	mux.HandleFunc("/update", update)
 	mux.HandleFunc("/create", createReq)
 	mux.HandleFunc("/delete", delete)
+
+	// Настройка обработчика CORS
 	handler := cors.Default().Handler(mux)
+
+	// Запуск сервера
 	err := http.ListenAndServe(PORT, handler)
 	if err != nil {
 		log.Println(err)
